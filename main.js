e9ce7f0fc351470b392144f9f3a9b8f70eaae2ec
Jeff Vincent <34778075+jeff-vincent@users.noreply.github.com>
2019-01-03 20:42:52 -0800

    Add README
    Init
    
    Init
    
    Fix spacing

---
+++ main.js
@@ -0,0 +1,43 @@
+const unirest = require('unirest');
+
+const API_KEY = "YOUR_API_KEY";
+const INGREDIENT_LIST = ['bananas', 'apples', 'cheese', 'crackers'];
+
+let requestString = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/find" +
+"ByIngredients?number=5&ranking=1&ingredients=";
+
+const ingredientsString = INGREDIENT_LIST.map(ingredient => 
+    ingredient + '%2C'
+);
+
+requestString = requestString + ingredientsString;
+
+unirest.get(requestString)
+.header("X-RapidAPI-Key",  API_KEY)
+.end(function (result) {
+    if (result.status === 200){
+        getRecipeData(result.body);
+    };
+});
+
+function getRecipeData(recipeList) {
+    console.log(recipeList);
+    let idList = [];
+    recipeList.map(recipe => 
+        idList.push(recipe.id)
+    );
+    let recipeString = idList.join('%2C');
+    let requestString = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/informationBulk?ids=';
+    requestString = requestString + recipeString
+    unirest.get(requestString)
+    .header("X-RapidAPI-Key", API_KEY)
+    .end(function (result) {
+         if(result.status === 200){
+             result.body.map(result =>
+                 console.log(
+                     result.title + " -- " + "Weight Watchers Smart Points: " + result.weightWatcherSmartPoints
+                 )
+             )
+         };
+    });    
+};
